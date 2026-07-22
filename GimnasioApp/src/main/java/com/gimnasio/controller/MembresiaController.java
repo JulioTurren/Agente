1e1d698c4788a5825a2fdbc62a9c585c833ff789
Julio Turren <julioturren@gmail.com>
2026-07-21 18:25:56 -0600

    Formulario de pagos, Correcion de vista de membresias , Entrenamiento del bot

--- GimnasioApp/src/main/java/com/gimnasio/controller/MembresiaController.java
+++ GimnasioApp/src/main/java/com/gimnasio/controller/MembresiaController.java
@@ -22,7 +22,7 @@ public class MembresiaController {
         this.membresiaService = membresiaService;
         this.userService = userService;
     }
-
+/* 
     @GetMapping
     public String showMembresiaPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
         User user = userService.findByEmail(userDetails.getUsername());
@@ -33,7 +33,28 @@ public class MembresiaController {
         model.addAttribute("tipos", Membresia.MembresiaType.values());
         return "membership";
     }
+*/
+@GetMapping
+public String showMembresiaPage(@AuthenticationPrincipal UserDetails userDetails, Model model) {
 
+    System.out.println("Entró al controlador");
+
+    System.out.println("UserDetails: " + userDetails);
+
+    User user = userService.findByEmail(userDetails.getUsername());
+
+    System.out.println("User: " + user);
+
+    Membresia membresia = membresiaService.findByUserId(user.getId()).orElse(null);
+
+    System.out.println("Membresia: " + membresia);
+
+    model.addAttribute("user", user);
+    model.addAttribute("membresia", membresia);
+    model.addAttribute("tipos", Membresia.MembresiaType.values());
+
+    return "membership";
+}
     @PostMapping("/suscribir")
     public String suscribir(@RequestParam Membresia.MembresiaType tipo,
                             @AuthenticationPrincipal UserDetails userDetails,
